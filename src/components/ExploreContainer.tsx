1fdaf3c1174f694a345a6d90735aeb8313f0891f
jayson14ify <rheakeah07@gmail.com>
2024-03-05 08:48:28 +0800

    add picture

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,21 @@
 import './ExploreContainer.css';
+import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonCard>
+    <img alt="Hello World!" src="https://miro.medium.com/v2/resize:fit:1400/1*veRna0Vbw1_4OUnzW9rSzA.jpeg" />
+    <IonCardHeader>
+      <IonCardTitle>Hello World</IonCardTitle>
+      <IonCardSubtitle>Wa ra gud</IonCardSubtitle>
+    </IonCardHeader>
+
+    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+  </IonCard>
   );
 };
 
